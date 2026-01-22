6b9802d1b94cc9a9441a70d79598df685bc509d1
Arijit Das <arijitdas.das99@gmail.com>
2026-01-22 13:45:45 +0530

    operators

---
+++ Operators/Arithmatic.py
@@ -0,0 +1,25 @@
+"""
+Docstring for Operators.Arithmatic
+
++ = addition
+
+- = subtraction
+
+* = multiplication
+
+// = floor division
+
+% = integer division
+
+** = exponentian
+
+"""
+
+a = 20
+b = 30
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(a**b)
---
+++ Operators/Assignment.py
@@ -0,0 +1,34 @@
+"""
+Docstring for Operators.Assignment
+
+=
+
++=
+
+-=
+
+*=
+
+//=
+
+%=
+
+**=
+
+:= walrus operator
+
+"""
+
+a = 20
+a += 10
+a -= 10
+a *= 20
+a //= 10
+a**=2
+
+print(a)
+
+b = 10
+print(b)
+
+print(b := 10)
