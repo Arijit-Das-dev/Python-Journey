5bee00ff7b2b4bc955fe0990ec67aedfabdb9a2c
Arijit Das <arijitdas.das99@gmail.com>
2026-01-23 16:32:42 +0530

    short hand

---
+++ Control_Statements/Short_Hand_If.py
@@ -0,0 +1,13 @@
+a = 20
+
+if a>10: print("a is greater")
+
+
+b = 20
+print("b is greater") if b>10 else print("a is greater")
+
+
+username = ""
+
+result = "error" if len(username) <= 0 else "Okay"
+print("result : ",result)
