07c24cfb55d9159a5bd23e4f406fb798be1ed75e
Arijit Das <arijitdas.das99@gmail.com>
2026-01-23 13:02:25 +0530

    List slicing

---
+++ List_Data_Structure/ListSlicing.py
@@ -0,0 +1,13 @@
+#  List Slicng
+
+nums = [12, 23, 45, 67, 89]
+
+# Positive slicing
+print(nums[1:3])
+print(nums[0:2])
+print(nums[3:])
+
+
+# Negative slicing
+print(nums[-1:])
+print(nums[-5:-2])
