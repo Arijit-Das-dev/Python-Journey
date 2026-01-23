c965ff384d90ece784db54fc848a08b13472a21c
Arijit Das <arijitdas.das99@gmail.com>
2026-01-23 13:42:14 +0530

    List methods

---
+++ List_Data_Structure/ListMethods.py
@@ -0,0 +1,33 @@
+nums = [1, 2, 3, 4]
+
+# append()
+nums.append(5)
+print(nums)
+
+
+# insert()
+nums.insert(1, 5)
+print(nums)
+
+
+# index()
+print(nums.index(5))
+
+
+# count()
+print(nums.count(2))
+
+
+# pop()
+nums.pop()
+
+
+# copy()
+newNum = nums.copy()
+print(newNum)
+
+
+# remove()
+
+nums.remove(1)
+print(nums)
