b5473eef1565acabcf00e7c6a699e3fa611066e3
Arijit Das <arijitdas.das99@gmail.com>
2026-01-24 16:28:38 +0530

    Nested For loop

---
+++ Loops/NestedForLoop.py
@@ -0,0 +1,21 @@
+"""
+Docstring for Loops.NestedForLoop
+"""
+
+nums = [1, 2, 3, 4, 5]
+chars = ["a", "b", "c", "d"]
+
+for num in nums:
+    for char in chars:
+        print(num, char)
+
+
+
+nums = [1, 2, 3, 4]
+chars = ["a", "b", "c", "d"]
+numChars = ["1a", "2b", "3c", "4d"]
+
+for num in nums:
+    for char in chars:
+        for numChar in numChars:
+            print(num, char, numChar)
