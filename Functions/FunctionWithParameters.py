6a846339ed5252cffd8baa2e44eeb1dd9db9b194
Arijit Das <arijitdas.das99@gmail.com>
2026-01-24 16:55:47 +0530

    FunctionWithParameters

---
+++ Functions/FunctionWithParameters.py
@@ -0,0 +1,19 @@
+"""
+Docstring for Functions.FunctionWithParameters
+"""
+
+def parameters(a, b):
+
+    summ = a + b
+
+    return summ
+
+result = parameters(10, 12)
+print(result)
+
+
+def greet(name):
+
+    print("hello",name)
+
+greet("arijit")
