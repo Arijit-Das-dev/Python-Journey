af2c82c709f592c09ecd507cdb4c458ef7a01263
Arijit Das <arijitdas.das99@gmail.com>
2026-01-24 17:13:05 +0530

    FunctionWithParameters

--- Functions/FunctionWithParameters.py
+++ Functions/FunctionWithParameters.py
@@ -2,6 +2,7 @@
 Docstring for Functions.FunctionWithParameters
 """
 
+# With Parameters
 def parameters(a, b):
 
     summ = a + b
@@ -17,3 +18,31 @@ def greet(name):
     print("hello",name)
 
 greet("arijit")
+
+
+# Default arguements
+def example(name = "default"):
+
+    print("Hello",name)
+
+example("Arijit")
+
+
+# Keyword arguements <= when we pass the actual parameter name with it's value 
+def pet(animalName, colour, breed):
+
+    print("My animal name is",animalName)
+    print("My", animalName,"'s" ,"colour is", colour)
+    print("My dog's breed is",breed)
+
+pet(animalName="Dog", colour="Black", breed="Pitbull")
+
+
+# Positional arguements <= where we dont need to pass any actual parameter name, only the value
+def pet(animalName, colour, breed):
+
+    print("My animal name is",animalName)
+    print("My", animalName,"'s" ,"colour is", colour)
+    print("My dog's breed is",breed)
+
+pet("Dog", "Black", "Pitbull")
