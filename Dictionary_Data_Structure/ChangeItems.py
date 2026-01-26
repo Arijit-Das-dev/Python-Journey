983bdc98307302879b92237cc406a5c6673e6d15
Arijit Das <arijitdas.das99@gmail.com>
2026-01-26 15:43:38 +0530

    change items

---
+++ Dictionary_Data_Structure/ChangeItems.py
@@ -0,0 +1,19 @@
+"""
+Docstring for Dictionary_Data_Structure.ChangeItems
+"""
+std_info = {
+
+    "name" : "arijit",
+    "id" : 1234,
+    "dept" : "BCA",
+    "sec" : "3Q",
+    "score": 90
+    
+}
+
+std_info["name"] = "a"
+std_info["id"] = 53190
+std_info["dept"] = "bba"
+std_info["score"] = 908
+
+print(std_info)
