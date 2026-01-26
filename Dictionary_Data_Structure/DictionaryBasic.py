bdb72d43ede9acf7132946a46311005e75aaa03f
Arijit Das <arijitdas.das99@gmail.com>
2026-01-26 15:26:36 +0530

    Dictionary Basics

---
+++ Dictionary_Data_Structure/DictionaryBasic.py
@@ -0,0 +1,30 @@
+"""
+Docstring for Dictionary_Data_Structure.DictionaryBasic
+"""
+example = {
+
+    "string" : "abcde",
+    "integer" : 12,
+    "float" : 12.0,
+    "boolean": True
+
+}
+print(example)
+
+std_info = {
+
+    "name" : "arijit",
+    "id" : 1234,
+    "dept" : "BCA",
+    "sec" : "3Q",
+    "score": 90
+    
+}
+print(std_info)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
