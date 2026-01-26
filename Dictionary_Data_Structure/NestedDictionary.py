4e83bdcf64f56ca3761ecb03f57b3bc3352bf368
Arijit Das <arijitdas.das99@gmail.com>
2026-01-26 16:13:25 +0530

    Nested Dictionary

---
+++ Dictionary_Data_Structure/NestedDictionary.py
@@ -0,0 +1,17 @@
+thisDict = {
+    "child 1":{
+        "name":"a",
+        "age" : 12
+    },
+    "child 2" : {
+        "name" : "b",
+        "age" : 21
+    },
+    "child 3":{
+        "name":"c",
+        "age":15
+    }
+}
+print(thisDict)
+
+
