e4ae1b83ce0e91f94fbf647c88324506d8d0a590
Arijit Das <arijitdas.das99@gmail.com>
2026-01-26 14:45:11 +0530

    **kwargs

--- Functions/ArbitoryArguements.py
+++ Functions/ArbitoryArguements.py
@@ -52,3 +52,23 @@ def division(*nums):
     return result
 
 print(division(12, 6))
+
+
+
+# def function(**kwargs):
+
+
+def my_function(**myvar):
+  print("Type:", type(myvar))
+  print("Name:", myvar["name"])
+  print("Age:", myvar["age"])
+  print("All data:", myvar)
+
+my_function(name = "Tobias", age = 30, city = "Bergen")
+
+def my_function(title, *args, **kwargs):
+  print("Title:", title)
+  print("Positional arguments:", args)
+  print("Keyword arguments:", kwargs)
+
+my_function("User Info", "Emil", "Tobias", age = 25, city = "Oslo")
