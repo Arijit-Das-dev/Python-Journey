d5e5309160606a3577f9655d90e2f47bb270c00f
Arijit Das <arijitdas.das99@gmail.com>
2026-01-31 19:05:59 +0530

    Updated

--- Functions/LambdaFunction.py
+++ Functions/LambdaFunction.py
@@ -6,6 +6,7 @@ A lambda function is a small anonymous function.
 A lambda function can take any number of arguments, but can only have one expression.
 """
 
+# BASIC LAMBDA FUNCTION
 add = lambda a : a + 10
 print(add(2))
 
@@ -17,3 +18,28 @@ print(mul(1, 2, 4))
 
 div = lambda a, b : a/b
 print(div(4, 2))
+
+
+# LAMBDA FUNCTION INSIDE A FUNCTION
+"""
+In this case we are defining two functions =>
+
+1. Outer function
+2. Inner lambda function
+
+**Note = we have to pass two actual parameter
+"""
+def add(n):
+
+    return lambda a: a*n
+
+result = add(5)
+print(result(5))
+
+
+def myFunction(n):
+
+    return lambda a : a/n
+
+result = myFunction(5)
+print(result(5))
