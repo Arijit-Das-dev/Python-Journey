8aa38bd228312bc68351ad609c57b70eaab792eb
Arijit Das <arijitdas.das99@gmail.com>
2026-01-31 18:56:56 +0530

    Updated

--- Functions/LambdaFunction.py
+++ Functions/LambdaFunction.py
@@ -6,5 +6,14 @@ A lambda function is a small anonymous function.
 A lambda function can take any number of arguments, but can only have one expression.
 """
 
-result = lambda a : a+ 10
-print(result(2))
+add = lambda a : a + 10
+print(add(2))
+
+sub = lambda a, b  : a-b
+print(sub(1, 2))
+
+mul = lambda a, b, c : a*b*c
+print(mul(1, 2, 4))
+
+div = lambda a, b : a/b
+print(div(4, 2))
