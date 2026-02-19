81525959cfec897902433481f647e38714620fb8
Arijit Das <arijitdas.das99@gmail.com>
2026-02-19 11:57:08 +0530

    updated

---
+++ File Handling/Modes/write.py
@@ -0,0 +1,10 @@
+"""
+Docstring for File Handling.Modes.write
+
+Writing text into that existing file
+
+Used Mode : "w"
+
+**Note : By using "w" mode, you can create any non-existing file and append some text data into it
+"""
+
