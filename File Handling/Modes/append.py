dc99e58ff80ad1c82e89665584d324e876b4cf87
Arijit Das <arijitdas.das99@gmail.com>
2026-02-19 13:56:08 +0530

    updated

--- File Handling/Modes/append.py
+++ File Handling/Modes/append.py
@@ -6,6 +6,7 @@ appending some text into a existing file
 Used Mode : "a"
 
 **Note : All the text will be append at the end of all text of that file
+**Note : If the file does not exist, python automatically create that file
 """
 
 f = open("File Handling/Modes/New_text_file.txt", "a")
